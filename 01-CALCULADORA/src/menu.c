cdcd867619052c72a8033de6f74b6fc1352fcc2a
tales1982 <142450583+TalesPortfolio@users.noreply.github.com>
2025-06-09 10:39:57 +0200

    calculadora

---
+++ 01-CALCULADORA/src/menu.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   menu.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tales <tales@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/09 10:23:30 by tales             #+#    #+#             */
+/*   Updated: 2025/06/09 10:28:16 by tales            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "calculadora.h"
+
+void menu()
+{
+    printf("===============================\n");
+    printf("        Calculadora Simples    \n");
+    printf("===============================\n");
+    printf("Selecione uma operação:\n");
+    printf("1. Adição\n");
+    printf("2. Subtração\n");
+    printf("3. Multiplicação\n");
+    printf("4. Divisão\n");
+    printf("5. Sair\n");
+    printf("Opção: ");
+}
+
